4773082c24eeff857fd16589976c9ce3c83cc91a
Bran David Ionel <bran.david96@yahoo.com>
2020-09-10 21:07:54 +0300

    test get data from api url

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,53 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Bran David-Ionel App  Vue.js for  WebMagnat "/>
+
+
+  <ul>
+    
+  <li v-for="todo of todos" :key="todo.id"> {{todo.service_status}}</li>  
+  </ul>
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from './components/HelloWorld.vue';
+import axios from 'axios';
+//make a test with a local db.json
+//const baseUrl="http://localhost:3000/totos";
+//rest api from app WebMagnatRestApi
+const baseUrl="http://127.0.0.1:8000/api/statuses";
 export default {
   name: 'App',
+  data(){
+    return{
+      todos:[{
+        service_status:''
+
+      }]
+    };
+
+  },
+  async created(){
+    try{
+      
+    const res=await axios.get(baseUrl);
+    this.todos=res.data;
+    }catch(e){
+      console.error(e);
+    }
+  },
   components: {
     HelloWorld
+  }/*,
+  mounted:function(){
+    axios.get('http://127.0.0.1:8000/api/statuses')
+    .then(response=>console.log(response));
+
   }
+  */
 }
 </script>
 
