638736355da87ad2cfed882056a7820bee423b4c
Bran David Ionel <bran.david96@yahoo.com>
2020-09-11 23:05:44 +0300

    I added the average

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,20 @@
 <input type="radio" id="bad" name="status" value="bad" v-model="status">
 <label for="bad">Bad</label><br>
 </div>
+<!--
 <h3>Other</h3>
 <input type="text" v-model="status" @keyup.enter="addStatus"><br>
+-->
 <button @click="addStatus">Submit</button>
+
+<h2>The results are:</h2>
   <ul>    
-  <li v-for="todo of todos" :key="todo.id"> {{todo.service_status}}</li>  
+  <li v-for="todo of todos" :key="todo.id"> 
+    {{todo.service_status}}-{{todo.nrStatuses}}  results! 
+  </li>  
   </ul>
+
+
   </div>
   
 </template>
@@ -30,22 +38,27 @@ import axios from 'axios';
 //make a test with a local db.json
 //const baseUrl="http://localhost:3000/totos";
 //rest api from app WebMagnatRestApi
-const baseUrlGet="http://127.0.0.1:8000/api/statuses";
+//all statuses 
+//const baseUrlGet="http://127.0.0.1:8000/api/statuses";
+//post de status 
 const baseUrlPost="http://127.0.0.1:8000/api/status";
+//get group status
+const baseUrlGroupedStatus="http://127.0.0.1:8000/api/grouped_status";
 export default {
   name: 'App',
   data(){
     return{
       todos:[{
       }],
-      status:''
+      status:'',
+      check_submit:false
     };
 
   },
   async created(){
     try{
       
-    const res=await axios.get(baseUrlGet);
+    const res=await axios.get(baseUrlGroupedStatus);
     this.todos=res.data;
     }catch(e){
       console.error(e);
@@ -55,8 +68,11 @@ export default {
   methods:{
     async addStatus(){
      try{
-      const res=await axios.post(baseUrlPost,{service_status:this.status});
-      this.todos=[...this.todos,res.data];
+      await axios.post(baseUrlPost,{service_status:this.status});
+      //I get the data what I post 
+      //this.todos=[...this.todos,res.data];
+      const res2=await axios.get(baseUrlGroupedStatus);
+      this.todos=res2.data;
       this.status='';
       }catch(e){
         console.error(e);
